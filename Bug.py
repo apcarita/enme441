824cce54cb35697769df1805df15661922314abb
apcarita <adamcarita@gmail.com>
2025-10-21 19:28:28 +0100

    pid

--- Bug.py
+++ Bug.py
@@ -2,26 +2,50 @@ import time
 from Shifter import Shifter
 import random as r
 import RPi.GPIO as GPIO
+import threading
 
-shifts = Shifter(2,3,4)
+class Bug():
+    def __init__(self,  shifter, timestep=0.1, isWrapOn=False, x=3):
+        self.timestep = timestep
+        self.isWrapOn = isWrapOn
+        self.__shifter = shifter
+        self.x = x
+        self.go = False
+        self.thread = None
+    
+    def _run(self):
+        while(self.go):
+            dir = r.randint(0,1) 
 
-pattern = 0b00100000
+            if(dir):
+                self.x = self.x >> 1
+                if(self.x <= 0):
+                    if(self.isWrapOn):
+                        self.x = 128
+                    else:
+                        self.x=1
+            else:
+                self.x = self.x << 1
+                if(self.x >= 128):
+                    if(self.isWrapOn):
+                        self.x = 1
+                    else:
+                        self.x=128
+            
+            self.__shifter.shiftByte(self.x)
+            time.sleep(self.timestep)
 
-try:
-    while 1:
+    def start(self):
+        if(not self.go):
+            self.go = True
+            self.thread = threading.Thread(target=self._run)
+            self.thread.start()
 
-        dir = r.randint(0,1) -1
-        print(f"moving {dir}")
+    def stop(self):
+        self.go = False
+        if self.thread:
+            self.thread.join()
+        self.__shifter.shiftByte(0)
 
-        if(dir):
-            pattern >> 1
-        else:
-            pattern << 1
-        
-        shifts.shiftByte(pattern)
-        time.sleep(0.5)
-
-
-except:
-    shifts.clean()
-    GPIO.cleanup()
+    
+    
